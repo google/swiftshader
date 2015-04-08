6074f126aaaae75846d63c787857f5293ed0d27f
Greg Hartman <ghartman@google.com>
2015-04-08 09:57:16 -0700

    Treat the call to getIntegerv as a non-error on Android only.
    Change-Id: I8aab90d46971bc47ab14a8b3df573637f8a0106f
    Reviewed-on: https://swiftshader-review.googlesource.com/2820
    Tested-by: Greg Hartman <ghartman@google.com>
    Reviewed-by: Greg Hartman <ghartman@google.com>
    Reviewed-by: Nicolas Capens <capn@google.com>

--- src/OpenGL/libGLESv2/libGLESv2.cpp
+++ src/OpenGL/libGLESv2/libGLESv2.cpp
@@ -33,6 +33,10 @@
 
 #include <limits>
 
+#ifdef ANDROID
+#include <cutils/log.h>
+#endif
+
 typedef std::pair<GLenum, GLenum> InternalFormatTypePair;
 typedef std::map<InternalFormatTypePair, GLenum> FormatMap;
 
@@ -2718,7 +2722,12 @@ void GL_APIENTRY glGetIntegerv(GLenum pname, GLint* params)
 
 	if(!context)
 	{
-		ERR("glGetIntegerv() called without current context.");   // Not strictly an error, but probably unintended or attempting to rely on non-compliant behavior
+		// Not strictly an error, but probably unintended or attempting to rely on non-compliant behavior
+		#ifdef __ANDROID__
+			ALOGI("expected_badness glGetIntegerv() called without current context.");
+		#else
+			ERR("glGetIntegerv() called without current context.");
+		#endif
 
 		// This is not spec compliant! When there is no current GL context, functions should
 		// have no side effects. Google Maps queries these values before creating a context,
