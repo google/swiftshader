be381599767d71d2b92ebfca5440468185dceaa3
Alexis Hetu <sugoi@google.com>
2015-06-30 11:03:03 -0400

    Added proper size/offset checks
    glTexSubImage3D and glCopyTexSubImage3D were missing
    some validity checks for size and offset parameters.
    
    Change-Id: Iff1aa034318c1fc58f9bb433c61bd8623493604a
    Reviewed-on: https://swiftshader-review.googlesource.com/3602
    Tested-by: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Nicolas Capens <capn@google.com>

--- src/OpenGL/libGLESv2/libGLESv3.cpp
+++ src/OpenGL/libGLESv2/libGLESv3.cpp
@@ -642,7 +642,7 @@ GL_APICALL void GL_APIENTRY glTexSubImage3D(GLenum target, GLint level, GLint xo
 		return error(GL_INVALID_VALUE);
 	}
 
-	if((width < 0) || (height < 0) || (depth < 0))
+	if((width < 0) || (height < 0) || (depth < 0) || (xoffset < 0) || (yoffset < 0) || (zoffset < 0))
 	{
 		return error(GL_INVALID_VALUE);
 	}
@@ -676,6 +676,11 @@ GL_APICALL void GL_APIENTRY glCopyTexSubImage3D(GLenum target, GLint level, GLin
 	}
 
 	if((level < 0) || (level >= es2::IMPLEMENTATION_MAX_TEXTURE_LEVELS))
+	{
+		return error(GL_INVALID_VALUE);
+	}
+
+	if((width < 0) || (height < 0) || (xoffset < 0) || (yoffset < 0) || (zoffset < 0))
 	{
 		return error(GL_INVALID_VALUE);
 	}
