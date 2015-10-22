b7e6c326983ab0377c4e2a4789fcf25125011d6b
Nicolas Capens <capn@google.com>
2015-10-21 20:25:15 -0400

    Simplify Android window property queries.
    Change-Id: Ic54eefe422f5d0478b7cdd8dfaa19ccb2e1a8a8e
    Reviewed-on: https://swiftshader-review.googlesource.com/4131
    Tested-by: Nicolas Capens <capn@google.com>
    Reviewed-by: Nicolas Capens <capn@google.com>

--- src/OpenGL/libEGL/Surface.cpp
+++ src/OpenGL/libEGL/Surface.cpp
@@ -230,22 +230,6 @@ WindowSurface::WindowSurface(Display *display, const Config *config, EGLNativeWi
     frameBuffer = nullptr;
 }
 
-#ifdef __ANDROID__
-static int32_t getWindowProp(ANativeWindow* window, int what) {
-    int value;
-    int res = window->query(window, what, &value);
-    return res < 0 ? res : value;
-}
-
-static int32_t ANativeWindow_getHeight(ANativeWindow* window) {
-    return getWindowProp(window, NATIVE_WINDOW_HEIGHT);
-}
-
-static int32_t ANativeWindow_getWidth(ANativeWindow* window) {
-  return getWindowProp(window, NATIVE_WINDOW_WIDTH);
-}
-#endif
-
 bool WindowSurface::initialize()
 {
     ASSERT(!frameBuffer && !backBuffer && !depthStencil);
@@ -256,7 +240,10 @@ bool WindowSurface::initialize()
 
 		return reset(windowRect.right - windowRect.left, windowRect.bottom - windowRect.top);
 	#elif defined(__ANDROID__)
-		return reset(ANativeWindow_getWidth(window), ANativeWindow_getHeight(window));
+		int width;  window->query(window, NATIVE_WINDOW_WIDTH, &width);
+		int height; window->query(window, NATIVE_WINDOW_HEIGHT, &height);
+
+		return reset(width, height);
 	#else
 		XWindowAttributes windowAttributes;
 		libX11->XGetWindowAttributes(display->getNativeDisplay(), window, &windowAttributes);
@@ -295,8 +282,8 @@ bool WindowSurface::checkForResize()
 		int clientWidth = client.right - client.left;
 		int clientHeight = client.bottom - client.top;
 	#elif defined(__ANDROID__)
-		int clientWidth = ANativeWindow_getWidth(window);
-		int clientHeight = ANativeWindow_getHeight(window);
+		int clientWidth;  window->query(window, NATIVE_WINDOW_WIDTH, &clientWidth);
+		int clientHeight; window->query(window, NATIVE_WINDOW_HEIGHT, &clientHeight);
 	#else
 		XWindowAttributes windowAttributes;
 		libX11->XGetWindowAttributes(display->getNativeDisplay(), window, &windowAttributes);
