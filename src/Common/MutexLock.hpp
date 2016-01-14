69c5c38757c8b07691cfbd1b7844d80c1d9fad70
Nicolas Capens <capn@google.com>
2016-01-14 10:45:55 -0500

    Fix Clang warnings.
    Change-Id: I9630093a14d0c1b2b6dc60ffb5866c7d2eaf4f02
    Reviewed-on: https://swiftshader-review.googlesource.com/4552
    Tested-by: Nicolas Capens <capn@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Nicolas Capens <capn@google.com>

--- src/Common/MutexLock.hpp
+++ src/Common/MutexLock.hpp
@@ -112,10 +112,14 @@ namespace sw
 		}
 
 	private:
-		// Ensure that the mutex variable is on its own 64-byte cache line to avoid false sharing
-		volatile int a[16];
-		volatile int mutex;
-		volatile int b[15];
+		struct
+		{
+			// Ensure that the mutex variable is on its own 64-byte cache line to avoid false sharing
+			// Padding must be public to avoid compiler warnings
+			volatile int padding1[16];
+			volatile int mutex;
+			volatile int padding2[15];
+		};
 	};
 }
 
