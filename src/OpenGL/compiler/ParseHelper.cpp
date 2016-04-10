13c8f61ffbf24e1a35c8dc75ef589bf8691b3f2e
Nicolas Capens <capn@google.com>
2016-04-10 01:54:50 -0400

    Fix constant expression vector indexing.
    Change-Id: If3b61ad65f1f1d6687890bd8715b2a534c78a353
    Reviewed-on: https://swiftshader-review.googlesource.com/5081
    Tested-by: Nicolas Capens <capn@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Nicolas Capens <capn@google.com>

--- src/OpenGL/compiler/ParseHelper.cpp
+++ src/OpenGL/compiler/ParseHelper.cpp
@@ -2616,7 +2616,7 @@ TIntermTyped *TParseContext::addFieldSelectionExpression(TIntermTyped *baseExpre
 			TIntermTyped *index = intermediate.addSwizzle(fields, fieldLocation);
 			indexedExpression = intermediate.addIndex(EOpVectorSwizzle, baseExpression, index, dotLocation);
 			indexedExpression->setType(TType(baseExpression->getBasicType(), baseExpression->getPrecision(),
-				EvqTemporary, (unsigned char)vectorString.size()));
+				baseExpression->getQualifier() == EvqConstExpr ? EvqConstExpr : EvqTemporary, (unsigned char)vectorString.size()));
 		}
 	}
 	else if(baseExpression->isMatrix())
