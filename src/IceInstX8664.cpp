fe2caab8cb0fecc764fdb710014b3a60b9d7b2d2
Manasij Mukherjee <manasijm@google.com>
2016-06-27 15:42:12 -0700

    Reorder X8664 Register Defs so that scratch registers are allocated first
    Reduces register pressure and the number of push/pops slightly.
    
    BUG=none
    R=stichnot@chromium.org
    
    Review URL: https://codereview.chromium.org/2100333002 .

--- src/IceInstX8664.cpp
+++ src/IceInstX8664.cpp
@@ -161,10 +161,10 @@ void TargetX8664Traits::X86OperandMem::emit(const Cfg *Func) const {
       const Type MemType = getType();
       if (Base->getType() != IceType_i32 && MemType != IceType_void) {
         // X86-64 is ILP32, but %rsp and %rbp are accessed as 64-bit registers.
-        // For filetype=asm, they need to be emitted as their 32-bit sibilings.
+        // For filetype=asm, they need to be emitted as their 32-bit siblings.
         assert(Base->getType() == IceType_i64);
-        assert(Base->getRegNum() == RegX8664::Encoded_Reg_rsp ||
-               Base->getRegNum() == RegX8664::Encoded_Reg_rbp ||
+        assert(getEncodedGPR(Base->getRegNum()) == RegX8664::Encoded_Reg_rsp ||
+               getEncodedGPR(Base->getRegNum()) == RegX8664::Encoded_Reg_rbp ||
                getType() == IceType_void);
         B = B->asType(Func, IceType_i32, X8664::Traits::getGprForType(
                                              IceType_i32, Base->getRegNum()));
