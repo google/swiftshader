96117fe78fcf6cd2f581eb883f4e2e5f75d74c6a
Nicolas Capens <capn@google.com>
2016-09-13 10:39:46 -0400

    Generate an arithmetic instruction.
    Change-Id: I3594fffc380b05fe25987afc4e572dbf017caa4b
    Reviewed-on: https://swiftshader-review.googlesource.com/7153
    Reviewed-by: Nicolas Capens <capn@google.com>
    Tested-by: Nicolas Capens <capn@google.com>

--- src/Reactor/Main.cpp
+++ src/Reactor/Main.cpp
@@ -12,11 +12,11 @@
 int main()
 {
 	Ice::ClFlags::Flags.setOutFileType(Ice::FT_Elf);
-	Ice::ClFlags::Flags.setOptLevel(Ice::Opt_m1);
+	Ice::ClFlags::Flags.setOptLevel(Ice::Opt_2);
 
 	std::unique_ptr<Ice::Ostream> cout(new llvm::raw_os_ostream(std::cout));
 	std::error_code errorCode;
-	std::unique_ptr<llvm::raw_fd_ostream> out(new llvm::raw_fd_ostream("out.o", errorCode, llvm::sys::fs::F_None));
+	std::unique_ptr<Ice::Fdstream> out(new Ice::Fdstream("out.o", errorCode, llvm::sys::fs::F_None));
 	std::unique_ptr<Ice::ELFStreamer> elf(new Ice::ELFStreamer(*out.get()));
 	Ice::GlobalContext context(cout.get(), cout.get(), cout.get(), elf.get());
 
@@ -24,13 +24,21 @@ int main()
 	{
 		Ice::CfgLocalAllocatorScope _(function.get());
 
-		function->setFunctionName(Ice::GlobalString::createWithString(&context, "Hello"));
-		function->setReturnType(Ice::IceType_void);
+		function->setFunctionName(Ice::GlobalString::createWithString(&context, "HelloWorld"));
+		Ice::Variable *arg1 = function->makeVariable(Ice::IceType_i32);
+		Ice::Variable *arg2 = function->makeVariable(Ice::IceType_i32);
+		function->addArg(arg1);
+		function->addArg(arg2);
+		function->setReturnType(Ice::IceType_i32);
 
 		Ice::CfgNode *node = function->makeNode();
 		function->setEntryNode(node);
 
-		Ice::InstRet *ret = Ice::InstRet::create(function.get());
+		Ice::Variable *sum = function->makeVariable(Ice::IceType_i32);
+		Ice::InstArithmetic *add = Ice::InstArithmetic::create(function.get(), Ice::InstArithmetic::Add, sum, arg1, arg2);
+		node->appendInst(add);
+
+		Ice::InstRet *ret = Ice::InstRet::create(function.get(), sum);
 		node->appendInst(ret);
 
 		function->translate();
