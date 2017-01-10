1fb0e50ea4ca069fd797faf07f342e0a126defed
Alexis Hetu <sugoi@google.com>
2017-01-10 16:58:04 -0500

    Adding swiftshader unittests to chromium
    Chromium does not allow any third_party code to depend on base,
    so the main test runner for swiftshader's tests in chromium can't
    live inside the swiftshader repo. To solve this:
    
    1) Added a test environment for swiftshader that will be used
    for testing in chromium. Currently, it is simply empty.
    2) Split unit tests in 2 files, unittests.cpp, which will
    contain the tests ran by both chromium and swiftshader and
    main.cpp, which will be used as the swiftshader test runner,
    but NOT as the chromium test runner (that one will be added
    in chromium at the same time I do the swiftshader update).
    3) The BUILD.gn file was updated to make the chromium version
    of the test depend on base and on the chromium version of the
    swiftshader test runner (swiftshader_tests_main.cc, to be added).
    
    Change-Id: I1128bb482565908bc285dcd6818d90d1d881a50e
    Reviewed-on: https://swiftshader-review.googlesource.com/8409
    Tested-by: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Nicolas Capens <capn@google.com>

--- tests/unittests/main.cpp
+++ tests/unittests/main.cpp
@@ -1,4 +1,4 @@
-// Copyright 2016 The SwiftShader Authors. All Rights Reserved.
+// Copyright 2017 The SwiftShader Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,11 +13,6 @@
 // limitations under the License.
 
 #include "gtest/gtest.h"
-
-TEST(SwiftShaderCompilationOnly, Unit)
-{
-	// Empty test to trigger compilation of SwiftShader on build bots
-}
 
 int main(int argc, char **argv)
 {
