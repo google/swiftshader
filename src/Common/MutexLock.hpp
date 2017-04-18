35e0ba7753cab88905b428b65577669620798507
Nicolas Capens <capn@google.com>
2017-04-18 11:20:01 -0400

    Use pthread mutexes on all Linux platforms.
    Bug chromium:710753
    
    Change-Id: Ie74d86de027417579c9c6949aabad1c3ae89ebfc
    Reviewed-on: https://swiftshader-review.googlesource.com/9329
    Reviewed-by: Corentin Wallez <cwallez@google.com>
    Reviewed-by: Nicolas Capens <capn@google.com>
    Tested-by: Nicolas Capens <capn@google.com>

--- src/Common/MutexLock.hpp
+++ src/Common/MutexLock.hpp
@@ -17,8 +17,8 @@
 
 #include "Thread.hpp"
 
-#ifdef __ANDROID__
-// Use an actual mutex on Android. Since many processes may use SwiftShader
+#if defined(__linux__)
+// Use a pthread mutex on Linux. Since many processes may use SwiftShader
 // at the same time it's best to just have the scheduler overhead.
 #include <pthread.h>
 
