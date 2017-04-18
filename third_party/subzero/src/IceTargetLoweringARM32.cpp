48a3fc74ee2425ad2ee3199f1401ff6cc623ba19
Nicolas Capens <capn@google.com>
2017-04-18 15:14:16 -0400

    Add intrinsic lowering stubs for ARM.
    Change-Id: I24a2ac536c24732538cc5c7f2aad4e7763989f9e
    Reviewed-on: https://chromium-review.googlesource.com/568673
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Jim Stichnoth <stichnot@chromium.org>
    Reviewed-on: https://swiftshader-review.googlesource.com/12690
    Tested-by: Nicolas Capens <nicolascapens@google.com>

--- third_party/subzero/src/IceTargetLoweringARM32.cpp
+++ third_party/subzero/src/IceTargetLoweringARM32.cpp
@@ -5304,11 +5304,59 @@ void TargetARM32::lowerIntrinsicCall(const InstIntrinsicCall *Instr) {
   case Intrinsics::Trap:
     _trap();
     return;
+  case Intrinsics::AddSaturateSigned: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::AddSaturateUnsigned: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
   case Intrinsics::LoadSubVector: {
     UnimplementedLoweringError(this, Instr);
     return;
   }
   case Intrinsics::StoreSubVector: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::MultiplyAddPairs: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::MultiplyHighSigned: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::MultiplyHighUnsigned: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::Nearbyint: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::Round: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::SignMask: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::SubtractSaturateSigned: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::SubtractSaturateUnsigned: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::VectorPackSigned: {
+    UnimplementedLoweringError(this, Instr);
+    return;
+  }
+  case Intrinsics::VectorPackUnsigned: {
     UnimplementedLoweringError(this, Instr);
     return;
   }
