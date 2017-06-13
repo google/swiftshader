f3c18a0aa65fc13edcad1f329f505e7d572f1fa1
Nicolas Capens <capn@google.com>
2017-06-13 14:37:07 -0400

    Add OpenGL ES 2.0 initialization unit tests.
    Create an OpenGL ES 2.0 context and verify the renderer string. This
    ensures the expected libGLESv2 get loaded and initialized correctly.
    Also add unittests to the Visual Studio solution.
    
    Change-Id: I848079215434ce1fb9a18564ce47de5b598b21d2
    Reviewed-on: https://swiftshader-review.googlesource.com/10108
    Tested-by: Nicolas Capens <capn@google.com>
    Reviewed-by: Nicolas Capens <capn@google.com>

--- tests/unittests/unittests.cpp
+++ tests/unittests/unittests.cpp
@@ -27,9 +27,9 @@ class SwiftShaderTest : public testing::Test
 protected:
 	void SetUp() override
 	{
-		#if defined(_WIN32)
+		#if defined(_WIN32) && !defined(STANDALONE)
 			// The DLLs are delay loaded (see BUILD.gn), so we can load
-			// the correct ones from the swiftshader subdirectory.
+			// the correct ones from Chrome's swiftshader subdirectory.
 			HMODULE libEGL = LoadLibraryA("swiftshader\\libEGL.dll");
 			EXPECT_NE((HMODULE)NULL, libEGL);
 
@@ -63,11 +63,130 @@ TEST_F(SwiftShaderTest, Initalization)
 	EXPECT_EQ(1, major);
 	EXPECT_EQ(4, minor);
 
-	const char *vendor = eglQueryString(display, EGL_VENDOR);
+	const char *eglVendor = eglQueryString(display, EGL_VENDOR);
 	EXPECT_EQ(EGL_SUCCESS, eglGetError());
-	EXPECT_STREQ("Google Inc.", vendor);
+	EXPECT_STREQ("Google Inc.", eglVendor);
 
-	const char *version = eglQueryString(display, EGL_VERSION);
+	const char *eglVersion = eglQueryString(display, EGL_VERSION);
 	EXPECT_EQ(EGL_SUCCESS, eglGetError());
-	EXPECT_THAT(version, testing::HasSubstr("1.4 SwiftShader "));
+	EXPECT_THAT(eglVersion, testing::HasSubstr("1.4 SwiftShader "));
+
+	eglBindAPI(EGL_OPENGL_ES_API);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+
+	const EGLint configAttributes[] =
+	{
+		EGL_SURFACE_TYPE,		EGL_PBUFFER_BIT,
+		EGL_RENDERABLE_TYPE,	EGL_OPENGL_ES2_BIT,
+		EGL_NONE
+	};
+
+	EGLConfig config;
+	EGLint num_config = -1;
+	EGLBoolean success = eglChooseConfig(display, configAttributes, &config, 1, &num_config);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ(num_config, 1);
+	EXPECT_EQ((EGLBoolean)EGL_TRUE, success);
+
+	EGLint conformant = 0;
+	eglGetConfigAttrib(display, config, EGL_CONFORMANT, &conformant);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_TRUE(conformant & EGL_OPENGL_ES2_BIT);
+
+	EGLint renderableType = 0;
+	eglGetConfigAttrib(display, config, EGL_RENDERABLE_TYPE, &renderableType);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_TRUE(renderableType & EGL_OPENGL_ES2_BIT);
+
+	EGLint surfaceType = 0;
+	eglGetConfigAttrib(display, config, EGL_RENDERABLE_TYPE, &surfaceType);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_TRUE(surfaceType & EGL_WINDOW_BIT);
+
+	EGLint surfaceAttributes[] =
+	{
+		EGL_WIDTH, 1920,
+		EGL_HEIGHT, 1080,
+		EGL_NONE
+	};
+
+	EGLSurface surface = eglCreatePbufferSurface(display, config, surfaceAttributes);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_NE(EGL_NO_SURFACE, surface);
+
+	EGLint contextAttributes[] =
+	{
+		EGL_CONTEXT_CLIENT_VERSION, 2,
+		EGL_NONE
+	};
+
+	EGLContext context = eglCreateContext(display, config, NULL, contextAttributes);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_NE(EGL_NO_SURFACE, surface);
+
+	success = eglMakeCurrent(display, surface, surface, context);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ((EGLBoolean)EGL_TRUE, success);
+
+	EGLDisplay currentDisplay = eglGetCurrentDisplay();
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_NE(EGL_NO_DISPLAY, currentDisplay);
+
+	EGLSurface currentDrawSurface = eglGetCurrentSurface(EGL_DRAW);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_NE(EGL_NO_SURFACE, currentDrawSurface);
+
+	EGLSurface currentReadSurface = eglGetCurrentSurface(EGL_READ);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_NE(EGL_NO_SURFACE, currentReadSurface);
+
+	EGLContext currentContext = eglGetCurrentContext();
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_NE(EGL_NO_CONTEXT, currentContext);
+
+	EXPECT_EQ((GLenum)GL_NO_ERROR, glGetError());
+
+	const GLubyte *glVendor = glGetString(GL_VENDOR);
+	EXPECT_EQ((GLenum)GL_NO_ERROR, glGetError());
+	EXPECT_STREQ("Google Inc.", (const char*)glVendor);
+
+	const GLubyte *glRenderer = glGetString(GL_RENDERER);
+	EXPECT_EQ((GLenum)GL_NO_ERROR, glGetError());
+	EXPECT_STREQ("Google SwiftShader", (const char*)glRenderer);
+
+	const GLubyte *glVersion = glGetString(GL_VERSION);
+	EXPECT_EQ((GLenum)GL_NO_ERROR, glGetError());
+	EXPECT_THAT((const char*)glVersion, testing::HasSubstr("OpenGL ES 2.0 SwiftShader "));
+
+	success = eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ((EGLBoolean)EGL_TRUE, success);
+
+//	currentDisplay = eglGetCurrentDisplay();
+//	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+//	EXPECT_EQ(EGL_NO_DISPLAY, currentDisplay);
+
+	currentDrawSurface = eglGetCurrentSurface(EGL_DRAW);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ(EGL_NO_SURFACE, currentDrawSurface);
+
+	currentReadSurface = eglGetCurrentSurface(EGL_READ);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ(EGL_NO_SURFACE, currentReadSurface);
+
+	currentContext = eglGetCurrentContext();
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ(EGL_NO_CONTEXT, currentContext);
+
+	success = eglDestroyContext(display, context);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ((EGLBoolean)EGL_TRUE, success);
+
+	success = eglDestroySurface(display, surface);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ((EGLBoolean)EGL_TRUE, success);
+
+	success = eglTerminate(display);
+	EXPECT_EQ(EGL_SUCCESS, eglGetError());
+	EXPECT_EQ((EGLBoolean)EGL_TRUE, success);
 }
