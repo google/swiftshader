99cb565a849193630e9850672b6477944da7f8aa
Nicolas Capens <capn@google.com>
2017-08-04 09:18:52 -0400

    Update eglplatform.h to match Android NDK.
    This change has been part of the Android NDK since API 18, corresponding
    with Android 4.3 Jelly Bean MR2.
    
    Change-Id: I6df06f46c286ce89ea56913684e43d558917d1a8
    Reviewed-on: https://swiftshader-review.googlesource.com/11269
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>

--- include/EGL/eglplatform.h
+++ include/EGL/eglplatform.h
@@ -85,8 +85,7 @@ typedef void *EGLNativePixmapType;
 
 #elif defined(__ANDROID__) || defined(ANDROID)
 
-#include <android/native_window.h>
-
+struct ANativeWindow;
 struct egl_native_pixmap_t;
 
 typedef struct ANativeWindow*           EGLNativeWindowType;
