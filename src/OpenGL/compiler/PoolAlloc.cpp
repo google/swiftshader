3d7c786fe99097aeeb195de77da8e9fab35d541d
Corentin Wallez <cwallez@google.com>
2017-10-31 18:05:38 -0400

    translator: Add define to disable pool allocation.
    This will help ASan find bugs in the translator when fuzzing, by making
    separate allocations instead of using suballocations within the pool.
    
    BUG=swiftshader:86
    
    Change-Id: Ic59f7eef2c7985f7bf545d41be956df478331e2d
    Reviewed-on: https://swiftshader-review.googlesource.com/13488
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>
    Tested-by: Corentin Wallez <cwallez@google.com>

--- src/OpenGL/compiler/PoolAlloc.cpp
+++ src/OpenGL/compiler/PoolAlloc.cpp
@@ -57,26 +57,15 @@ void SetGlobalPoolAllocator(TPoolAllocator* poolAllocator)
 // is documented in PoolAlloc.h.
 //
 TPoolAllocator::TPoolAllocator(int growthIncrement, int allocationAlignment) :
-	pageSize(growthIncrement),
-	alignment(allocationAlignment),
+	alignment(allocationAlignment)
+#if !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
+	, pageSize(growthIncrement),
 	freeList(0),
 	inUseList(0),
 	numCalls(0),
 	totalBytes(0)
+#endif
 {
-	//
-	// Don't allow page sizes we know are smaller than all common
-	// OS page sizes.
-	//
-	if (pageSize < 4*1024)
-		pageSize = 4*1024;
-
-	//
-	// A large currentPageOffset indicates a new page needs to
-	// be obtained to allocate memory.
-	//
-	currentPageOffset = pageSize;
-
 	//
 	// Adjust alignment to be at least pointer aligned and
 	// power of 2.
@@ -91,6 +80,20 @@ TPoolAllocator::TPoolAllocator(int growthIncrement, int allocationAlignment) :
 	alignment = a;
 	alignmentMask = a - 1;
 
+#if !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
+	//
+	// Don't allow page sizes we know are smaller than all common
+	// OS page sizes.
+	//
+	if (pageSize < 4*1024)
+		pageSize = 4*1024;
+
+	//
+	// A large currentPageOffset indicates a new page needs to
+	// be obtained to allocate memory.
+	//
+	currentPageOffset = pageSize;
+
 	//
 	// Align header skip
 	//
@@ -98,10 +101,14 @@ TPoolAllocator::TPoolAllocator(int growthIncrement, int allocationAlignment) :
 	if (headerSkip < sizeof(tHeader)) {
 		headerSkip = (sizeof(tHeader) + alignmentMask) & ~alignmentMask;
 	}
+#else  // !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
+	mStack.push_back({});
+#endif
 }
 
 TPoolAllocator::~TPoolAllocator()
 {
+#if !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
 	while (inUseList) {
 		tHeader* next = inUseList->nextPage;
 		inUseList->~tHeader();
@@ -118,6 +125,14 @@ TPoolAllocator::~TPoolAllocator()
 		delete [] reinterpret_cast<char*>(freeList);
 		freeList = next;
 	}
+#else  // !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
+	for (auto& allocs : mStack) {
+		for (auto alloc : allocs) {
+			free(alloc);
+		}
+	}
+	mStack.clear();
+#endif
 }
 
 // Support MSVC++ 6.0
@@ -158,14 +173,18 @@ void TAllocation::checkGuardBlock(unsigned char* blockMem, unsigned char val, co
 
 void TPoolAllocator::push()
 {
+#if !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
 	tAllocState state = { currentPageOffset, inUseList };
 
-	stack.push_back(state);
+	mStack.push_back(state);
 
 	//
 	// Indicate there is no current page to allocate from.
 	//
 	currentPageOffset = pageSize;
+#else  // !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
+	mStack.push_back({});
+#endif
 }
 
 //
@@ -177,11 +196,12 @@ void TPoolAllocator::push()
 //
 void TPoolAllocator::pop()
 {
-	if (stack.size() < 1)
+	if (mStack.size() < 1)
 		return;
 
-	tHeader* page = stack.back().page;
-	currentPageOffset = stack.back().offset;
+#if !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
+	tHeader* page = mStack.back().page;
+	currentPageOffset = mStack.back().offset;
 
 	while (inUseList != page) {
 		// invoke destructor to free allocation list
@@ -197,7 +217,13 @@ void TPoolAllocator::pop()
 		inUseList = nextInUse;
 	}
 
-	stack.pop_back();
+	mStack.pop_back();
+#else  // !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
+	for (auto alloc : mStack.back()) {
+		free(alloc);
+	}
+	mStack.pop_back();
+#endif
 }
 
 //
@@ -206,12 +232,13 @@ void TPoolAllocator::pop()
 //
 void TPoolAllocator::popAll()
 {
-	while (stack.size() > 0)
+	while (mStack.size() > 0)
 		pop();
 }
 
 void* TPoolAllocator::allocate(size_t numBytes)
 {
+#if !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
 	//
 	// Just keep some interesting statistics.
 	//
@@ -288,6 +315,14 @@ void* TPoolAllocator::allocate(size_t numBytes)
 	currentPageOffset = (headerSkip + allocationSize + alignmentMask) & ~alignmentMask;
 
 	return initializeAllocation(inUseList, ret, numBytes);
+#else  // !defined(SWIFTSHADER_TRANSLATOR_DISABLE_POOL_ALLOC)
+	void *alloc = malloc(numBytes + alignmentMask);
+	mStack.back().push_back(alloc);
+
+	intptr_t intAlloc = reinterpret_cast<intptr_t>(alloc);
+	intAlloc = (intAlloc + alignmentMask) & ~alignmentMask;
+	return reinterpret_cast<void *>(intAlloc);
+#endif
 }
 
 
