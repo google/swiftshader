e3490ede24844d5b2b79fff1cb84154be1b634a5
Nicolas Capens <capn@google.com>
2018-04-09 15:12:15 -0400

    Add work-arounds for missing C++11 features.
    Android versions before M did not have:
    
    std::shared_ptr
    std::unique_ptr
    std::to_string
    
    This is fixed by extending stlport with custom implementations for
    these features. This works by using 'string' and 'memory' header
    files in a path searched before stlport itself, in which the new
    features are implemented, and including stport's original headers for
    all other functionality.
    
    Bug b/75229322
    
    Change-Id: Icd05c072c366381155e086c3f14f805bc4f104d6
    Reviewed-on: https://swiftshader-review.googlesource.com/18408
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>

--- src/OpenGL/compiler/Android.mk
+++ src/OpenGL/compiler/Android.mk
@@ -12,7 +12,10 @@ COMMON_C_INCLUDES := \
 
 # Marshmallow does not have stlport, but comes with libc++ by default
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23 && echo PreMarshmallow),PreMarshmallow)
-COMMON_C_INCLUDES += external/stlport/stlport
+COMMON_C_INCLUDES += \
+	$(LOCAL_PATH)/../../../third_party/stlport-cpp11-extension/ \
+	external/stlport/stlport/ \
+	external/stlport/
 endif
 
 COMMON_CFLAGS := \
