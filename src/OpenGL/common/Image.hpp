a8bb28ca07e7dc2de2318d47f2be477d9911dcff
Jorge E. Moreira <jemoreira@google.com>
2018-04-24 17:05:10 -0700

    Use the correct constant name for the Android SDK version
    Bug b/78194651
    
    Change-Id: Iafe8728a3593b0bd48b9633261df4035d2e94117
    Reviewed-on: https://swiftshader-review.googlesource.com/18668
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>

--- src/OpenGL/common/Image.hpp
+++ src/OpenGL/common/Image.hpp
@@ -247,7 +247,7 @@ inline GLenum GLPixelFormatFromAndroid(int halFormat)
 #ifdef GRALLOC_MODULE_API_VERSION_0_2
 	case HAL_PIXEL_FORMAT_YCbCr_420_888: return SW_YV12_BT601;
 #endif
-#if PLATFORM_SDK_VERSION >= 26
+#if ANDROID_PLATFORM_SDK_VERSION >= 26
 	case HAL_PIXEL_FORMAT_RGBA_FP16: return GL_RGBA16F;
 #endif
 	case HAL_PIXEL_FORMAT_RGB_888:   // Unsupported.
