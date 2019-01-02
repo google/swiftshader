ec061ddc31ca9e0a01d006cf197272c7d1db71cf
Raymond Chiu <chiur@google.com>
2019-01-02 14:36:36 -0800

    Setup Kokoro linux build
    Bug b/123360006
    
    Change-Id: I90ebde7968135f136f010d1f526aca26bc2b9418
    Reviewed-on: https://swiftshader-review.googlesource.com/c/23988
    Reviewed-by: Chris Forbes <chrisforbes@google.com>
    Tested-by: Chris Forbes <chrisforbes@google.com>

---
+++ kokoro/gcp_ubuntu/continuous.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+cd git/SwiftShader
+
+git submodule update --init
+
+mkdir -p build && cd build
+
+cmake ..
+make --jobs=$(nproc)
