426b4c89ea87a11246cdfd1e711f2a67b9e09687
Nicolas Capens <capn@google.com>
2019-03-22 14:55:46 -0400

    Remove top-level kokoro/ folder
    We now use tests/kokoro/ instead.
    
    Bug b/126742833
    
    Change-Id: I50e41a6299238ca91ff92c0947b3060f9a840a5d
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/27789
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Chris Forbes <chrisforbes@google.com>

--- kokoro/gcp_windows/continuous.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-SET PATH=%PATH%;C:\python27
-
-cd git\SwiftShader
-
-git submodule update --init
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild" SwiftShader.sln
