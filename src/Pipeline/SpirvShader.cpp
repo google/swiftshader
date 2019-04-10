f41ca6b57107978252fdd14deaefe9fbc396c86c
Ben Clayton <bclayton@google.com>
2019-04-10 22:33:00 +0100

    SpirvShader: Fix unhelpful warning about parentheses on GCC
    Bug: b/123933266
    Change-Id: Ia7b59b1c6edbc2180ed7ff39ecce4bfdca20804a
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/28828
    Tested-by: Ben Clayton <bclayton@google.com>
    Kokoro-Presubmit: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>

--- src/Pipeline/SpirvShader.cpp
+++ src/Pipeline/SpirvShader.cpp
@@ -3762,7 +3762,7 @@ namespace sw
 		auto v = As<SIMD::UInt>(val);
 		auto isNotZero = CmpNEQ(v & SIMD::UInt(0x7FFFFFFF), SIMD::UInt(0));
 		auto zeroSign = v & SIMD::UInt(0x80000000) & ~isNotZero;
-		auto significand = As<SIMD::Float>((v & SIMD::UInt(0x807FFFFF) | SIMD::UInt(0x3F000000)) & isNotZero | zeroSign);
+		auto significand = As<SIMD::Float>((((v & SIMD::UInt(0x807FFFFF)) | SIMD::UInt(0x3F000000)) & isNotZero) | zeroSign);
 		auto exponent = Exponent(val) & SIMD::Int(isNotZero);
 		return std::make_pair(significand, exponent);
 	}
