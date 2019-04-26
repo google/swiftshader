1762c381c7fe7171cf0ccb855c8c38a6b0acf209
Ben Clayton <bclayton@google.com>
2019-04-26 22:56:07 +0100

    Kokoro: Build with ASAN enabled.
    Try and catch memory corruption in our unit tests.
    
    Change-Id: I5712c443de002ca563ba474cb77451d2791e7ec0
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/30169
    Tested-by: Ben Clayton <bclayton@google.com>
    Reviewed-by: Ben Clayton <bclayton@google.com>
    Kokoro-Presubmit: kokoro <noreply+kokoro@google.com>

--- tests/kokoro/macos/continuous.sh
+++ tests/kokoro/macos/continuous.sh
@@ -15,7 +15,7 @@ if [[ -z "${REACTOR_BACKEND}" ]]; then
   REACTOR_BACKEND="LLVM"
 fi
 
-cmake .. "-DREACTOR_BACKEND=${REACTOR_BACKEND}"
+cmake .. "-DASAN=ON -DREACTOR_BACKEND=${REACTOR_BACKEND}"
 make -j$(sysctl -n hw.logicalcpu)
 
 # Run the reactor unit tests.
