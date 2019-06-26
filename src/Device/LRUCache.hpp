6d874f057c1abb0af6f15bacada45e33beb9434a
Nicolas Capens <capn@google.com>
2019-06-26 10:31:47 -0400

    Selectively suppress class-memaccess warning/error
    To still produce the class-memaccess diagnostic on unintended cases,
    suppress it only for our Memset<T> case and LLVM.
    
    Bug: b/135744933
    Change-Id: Id3b421a3b831600097d198ac793f582467d0a520
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/33429
    Presubmit-Ready: Nicolas Capens <nicolascapens@google.com>
    Kokoro-Presubmit: kokoro <noreply+kokoro@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Ben Clayton <bclayton@google.com>

--- src/Device/LRUCache.hpp
+++ src/Device/LRUCache.hpp
@@ -55,7 +55,21 @@ namespace sw
 		Memset(T *object, int val)
 		{
 			static_assert(std::is_base_of<Memset<T>, T>::value, "Memset<T> must only clear the memory of a type of which it is a base class");
+
+			// GCC 8+ warns that
+			// "‘void* memset(void*, int, size_t)’ clearing an object of non-trivial type ‘T’;
+			//  use assignment or value-initialization instead [-Werror=class-memaccess]"
+			// This is benign iff it happens before any of the base or member constructrs are called.
+			#if defined(__GNUC__) && (__GNUC__ >= 8)
+			#pragma GCC diagnostic push
+			#pragma GCC diagnostic ignored "-Wclass-memaccess"
+			#endif
+
 			memset(object, 0, sizeof(T));
+
+			#if defined(__GNUC__) && (__GNUC__ >= 8)
+			#pragma GCC diagnostic pop
+			#endif
 		}
 	};
 
