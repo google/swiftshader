f3dce1123b3843b77f757db449e7eef5cea2a717
Alexis Hetu <sugoi@google.com>
2019-07-11 09:58:15 -0400

    Fix for rendering being too dark
    Using a non SRGB format with VK_COLOR_SPACE_SRGB_NONLINEAR_KHR
    yields a resulting image in the wrong color space. Making sure
    a surface format where format and color space are both SRGB
    fixes the issue.
    
    Bug b/137088174
    
    Change-Id: I934e6b60afe5f65d22832162e48aecef773627e8
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/34048
    Tested-by: Alexis Hétu <sugoi@google.com>
    Presubmit-Ready: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Chris Forbes <chrisforbes@google.com>

--- src/WSI/VkSurfaceKHR.cpp
+++ src/WSI/VkSurfaceKHR.cpp
@@ -24,6 +24,7 @@ namespace
 static const VkSurfaceFormatKHR surfaceFormats[] =
 {
 	{VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR},
+	{VK_FORMAT_B8G8R8A8_SRGB, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR},
 };
 
 static const VkPresentModeKHR presentModes[] =
