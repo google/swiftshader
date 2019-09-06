607908e29e24fe07087e9b19a92a2177d72765d8
Sean Risser <srisser@google.com>
2019-09-06 13:06:26 -0400

    Log unsupported extensions used in pNext structs
    The Vulkan spec states that if an implementation does not support an
    extension, then it must ignore all fields other than pNext and pType and
    continue on without processing any further information contained in the
    extension's structure. We've been using asserts to log these situations
    so we can tell which extension structs people are trying to use.
    
    This CL changes our behavior so we silently log these unsupported extensions,
    and warn a user once if they're debugging.
    
    Bug: b/139528538
    Change-Id: I8d31f147e22f670c9c10c64e239085700eae73a3
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/36089
    Kokoro-Presubmit: kokoro <noreply+kokoro@google.com>
    Tested-by: Sean Risser <srisser@google.com>
    Presubmit-Ready: Sean Risser <srisser@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>

--- src/Vulkan/VkDebug.cpp
+++ src/Vulkan/VkDebug.cpp
@@ -15,7 +15,75 @@
 #include "VkDebug.hpp"
 
 #include <string>
+#include <atomic>
 #include <stdarg.h>
+
+#if defined(__unix__)
+#define PTRACE
+#include <sys/types.h>
+#include <sys/ptrace.h>
+#elif defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#elif defined(__APPLE__) || defined(__MACH__)
+#include <unistd.h>
+#include <sys/sysctl.h>
+#endif
+
+namespace {
+
+bool IsUnderDebugger()
+{
+#if defined(PTRACE) && !defined(__APPLE__) && !defined(__MACH__)
+	static bool checked = false;
+	static bool res = false;
+
+	if (!checked)
+	{
+		// If a debugger is attached then we're already being ptraced and ptrace
+		// will return a non-zero value.
+		checked = true;
+		if (ptrace(PTRACE_TRACEME, 0, 1, 0) != 0)
+		{
+			res = true;
+		}
+		else
+		{
+			ptrace(PTRACE_DETACH, 0, 1, 0);
+		}
+	}
+
+	return res;
+#elif defined(_WIN32) || defined(_WIN64)
+	return IsDebuggerPresent() != 0;
+#elif defined(__APPLE__) || defined(__MACH__)
+	// Code comes from the Apple Technical Q&A QA1361
+
+	// Tell sysctl what info we're requestion. Specifically we're asking for
+	// info about this our PID.
+	int res = 0;
+	int request[4] = {
+		CTL_KERN,
+		KERN_PROC,
+		KERN_PROC_PID,
+		getpid()
+	};
+	struct kinfo_proc info;
+	size_t size = sizeof(info);
+
+	info.kp_proc.p_flag = 0;
+
+	// Get the info we're requesting, if sysctl fails then info.kp_proc.p_flag will remain 0.
+	res = sysctl(request, sizeof(request) / sizeof(*request), &info, &size, NULL, 0);
+	ASSERT_MSG(res == 0, "syscl returned %d", res);
+
+	// We're being debugged if the P_TRACED flag is set
+	return ((info.kp_proc.p_flag & P_TRACED) != 0);
+#else
+	return false;
+#endif
+}
+
+}
 
 namespace vk
 {
@@ -69,6 +137,31 @@ void abort(const char *format, ...)
 	va_end(vararg);
 
 	::abort();
+}
+
+void trace_assert(const char *format, ...)
+{
+	static std::atomic<bool> asserted = {false};
+	va_list vararg;
+	va_start(vararg, format);
+
+	if (IsUnderDebugger() && !asserted.exchange(true))
+	{
+		// Abort after tracing and printing to stderr
+		tracev(format, vararg);
+		va_end(vararg);
+
+		va_start(vararg, format);
+		vfprintf(stderr, format, vararg);
+		va_end(vararg);
+
+		::abort();
+	}
+	else if (!asserted)
+	{
+		tracev(format, vararg);
+		va_end(vararg);
+	}
 }
 
 }
