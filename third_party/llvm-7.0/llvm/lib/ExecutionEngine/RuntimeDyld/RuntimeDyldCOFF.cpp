561e9d62713608b982251d3f775e5a7b2012d06c
Adam Kallai <kadam@inf.u-szeged.hu>
2019-11-20 12:45:25 +0100

    ExecutionEngine: add preliminary support for COFF ARM64
    It was originally committed into LLVM: https://reviews.llvm.org/D69434
    
    This CL aims to continue the work on Swiftshader for Windows on ARM64.
    
    Bug: chromium:893460
    Change-Id: Ib0de220c1d332f2a16d2275a5f3aef4d1a53ee33
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/38453
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Tested-by: Alexis Hétu <sugoi@google.com>
    Kokoro-Presubmit: kokoro <noreply+kokoro@google.com>

--- third_party/llvm-7.0/llvm/lib/ExecutionEngine/RuntimeDyld/RuntimeDyldCOFF.cpp
+++ third_party/llvm-7.0/llvm/lib/ExecutionEngine/RuntimeDyld/RuntimeDyldCOFF.cpp
@@ -12,6 +12,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "RuntimeDyldCOFF.h"
+#include "Targets/RuntimeDyldCOFFAArch64.h"
 #include "Targets/RuntimeDyldCOFFI386.h"
 #include "Targets/RuntimeDyldCOFFThumb.h"
 #include "Targets/RuntimeDyldCOFFX86_64.h"
@@ -56,6 +57,8 @@ llvm::RuntimeDyldCOFF::create(Triple::ArchType Arch,
     return make_unique<RuntimeDyldCOFFThumb>(MemMgr, Resolver);
   case Triple::x86_64:
     return make_unique<RuntimeDyldCOFFX86_64>(MemMgr, Resolver);
+  case Triple::aarch64:
+    return make_unique<RuntimeDyldCOFFAArch64>(MemMgr, Resolver);
   }
 }
 
