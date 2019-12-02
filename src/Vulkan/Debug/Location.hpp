93433f078d1317778ddc9ec47328eaf2b06ac046
Ben Clayton <bclayton@google.com>
2019-12-02 20:09:09 +0000

    Vulkan/Debug: Add Location
    It holds a file path and line number.
    
    Bug: b/145351270
    Change-Id: I0f54d00bda151be529d912562061052abff2fe81
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/38896
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>
    Tested-by: Ben Clayton <bclayton@google.com>
    Kokoro-Presubmit: kokoro <noreply+kokoro@google.com>

---
+++ src/Vulkan/Debug/Location.hpp
@@ -0,0 +1,44 @@
+// Copyright 2019 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VK_DEBUG_LOCATION_HPP_
+#define VK_DEBUG_LOCATION_HPP_
+
+#include <memory>
+
+namespace vk
+{
+namespace dbg
+{
+
+class File;
+
+// Location holds a file path and line number.
+struct Location
+{
+	Location() = default;
+	inline Location(int line, const std::shared_ptr<File>& file);
+
+	int line = 0; // 1 based. 0 represents no line.
+	std::shared_ptr<File> file;
+};
+
+Location::Location(int line, const std::shared_ptr<File>& file) :
+    line(line),
+    file(file) {}
+
+}  // namespace dbg
+}  // namespace vk
+
+#endif  // VK_DEBUG_LOCATION_HPP_
