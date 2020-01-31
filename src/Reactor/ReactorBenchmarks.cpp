55890e1afe33d3fd47c9dd5c0e625e4d1b84d88b
Ben Clayton <bclayton@google.com>
2020-01-31 14:07:21 +0000

    CMake: Add build rules for google benchmark.
    Add a single Reactor Coroutine benchmark so there's something to compile.
    
    Bug: b/148660286
    Change-Id: Icbc4c44dd5c0f1ab495c3b8eddce6ea90199e987
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/40809
    Tested-by: Ben Clayton <bclayton@google.com>
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>

---
+++ src/Reactor/ReactorBenchmarks.cpp
@@ -0,0 +1,63 @@
+// Copyright 2020 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "Coroutine.hpp"
+#include "Reactor.hpp"
+
+#include "benchmark/benchmark.h"
+
+BENCHMARK_MAIN();
+
+class Coroutines : public benchmark::Fixture
+{
+public:
+	void SetUp(const ::benchmark::State &state) {}
+
+	void TearDown(const ::benchmark::State &state) {}
+};
+
+BENCHMARK_F(Coroutines, Fibonacci)
+(benchmark::State &state)
+{
+	using namespace rr;
+
+	if(!Caps.CoroutinesSupported)
+	{
+		state.SkipWithError("Coroutines are not supported");
+		return;
+	}
+
+	Coroutine<int()> function;
+	{
+		Yield(Int(0));
+		Yield(Int(1));
+		Int current = 1;
+		Int next = 1;
+		While(true)
+		{
+			Yield(next);
+			auto tmp = current + next;
+			current = next;
+			next = tmp;
+		}
+	}
+
+	auto coroutine = function();
+
+	int out = 0;
+	for(auto _ : state)
+	{
+		coroutine->await(out);
+	}
+}
