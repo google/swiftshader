db74776104f5ff6504c8d613f177517502f0e20f
Nicolas Capens <capn@google.com>
2020-03-31 12:00:46 -0400

    Update Kokoro's clang-format to version 10
    Note the .clang-format config file is now incompatible with clang-format
    version 8 due to the use of AfterCaseLabel, which is necessary to match
    the current formatting.
    
    Also add amaiorano@google.com as a project owner.
    
    Bug: b/144825072
    Change-Id: Ifd8ceae65245f0bc12143095ab5336f177f4a78f
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/43451
    Presubmit-Ready: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Kokoro-Result: kokoro <noreply+kokoro@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>

--- tests/kokoro/gcp_ubuntu/check_style.sh
+++ tests/kokoro/gcp_ubuntu/check_style.sh
@@ -5,12 +5,12 @@ set -x # Display commands being run.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 
 # Download clang tar
-CLANG_TAR="/tmp/clang-8.tar.xz"
-curl -L https://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz > ${CLANG_TAR}
+CLANG_TAR="/tmp/clang-10.tar.xz"
+curl -L https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > ${CLANG_TAR}
 # Verify clang tar
 sudo apt-get install pgpgpg
-gpg --import "${SCRIPT_DIR}/clang-8.pubkey.asc"
-gpg --verify "${SCRIPT_DIR}/clang-8.sig" ${CLANG_TAR}
+gpg --import "${SCRIPT_DIR}/hans-gpg-key.asc"
+gpg --verify "${SCRIPT_DIR}/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz.sig" ${CLANG_TAR}
 if [ $? -ne 0 ]
 then
   echo "clang download failed PGP check"
@@ -20,12 +20,12 @@ fi
 set -e # Fail on any error
 
 # Untar into tmp
-CLANG_DIR=/tmp/clang-8
+CLANG_DIR=/tmp/clang-10
 mkdir ${CLANG_DIR}
 tar -xf ${CLANG_TAR} -C ${CLANG_DIR}
 
 # Set up env vars
-export CLANG_FORMAT=${CLANG_DIR}/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-14.04/bin/clang-format
+export CLANG_FORMAT=${CLANG_DIR}/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04/bin/clang-format
 
 # Run presubmit tests
 cd git/SwiftShader
