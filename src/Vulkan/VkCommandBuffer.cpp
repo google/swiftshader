be1a9ac682873513930d2d3745d594893bd4a609
Nicolas Capens <capn@google.com>
2020-07-09 14:50:52 -0400

    Rename command classes to match API calls
    While e.g. "CmdImageToImageCopy" was slightly more explicit, it can
    leave one searching for the vkCmdImageToImageCopy API call.
    
    Bug: b/134584057
    Change-Id: I32d60e1389ec60c65496c35b9ac1d74a0e844c6f
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/46349
    Presubmit-Ready: Nicolas Capens <nicolascapens@google.com>
    Kokoro-Result: kokoro <noreply+kokoro@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>

--- src/Vulkan/VkCommandBuffer.cpp
+++ src/Vulkan/VkCommandBuffer.cpp
@@ -749,10 +749,10 @@ private:
 	uint32_t stride;
 };
 
-class CmdImageToImageCopy : public vk::CommandBuffer::Command
+class CmdCopyImage : public vk::CommandBuffer::Command
 {
 public:
-	CmdImageToImageCopy(const vk::Image *srcImage, vk::Image *dstImage, const VkImageCopy &region)
+	CmdCopyImage(const vk::Image *srcImage, vk::Image *dstImage, const VkImageCopy &region)
 	    : srcImage(srcImage)
 	    , dstImage(dstImage)
 	    , region(region)
@@ -764,7 +764,7 @@ public:
 		srcImage->copyTo(dstImage, region);
 	}
 
-	std::string description() override { return "vkCmdImageToImageCopy()"; }
+	std::string description() override { return "vkCmdCopyImage()"; }
 
 private:
 	const vk::Image *srcImage;
@@ -772,10 +772,10 @@ private:
 	const VkImageCopy region;
 };
 
-class CmdBufferToBufferCopy : public vk::CommandBuffer::Command
+class CmdCopyBuffer : public vk::CommandBuffer::Command
 {
 public:
-	CmdBufferToBufferCopy(const vk::Buffer *srcBuffer, vk::Buffer *dstBuffer, const VkBufferCopy &region)
+	CmdCopyBuffer(const vk::Buffer *srcBuffer, vk::Buffer *dstBuffer, const VkBufferCopy &region)
 	    : srcBuffer(srcBuffer)
 	    , dstBuffer(dstBuffer)
 	    , region(region)
@@ -787,7 +787,7 @@ public:
 		srcBuffer->copyTo(dstBuffer, region);
 	}
 
-	std::string description() override { return "vkCmdBufferToBufferCopy()"; }
+	std::string description() override { return "vkCmdCopyBuffer()"; }
 
 private:
 	const vk::Buffer *srcBuffer;
@@ -795,10 +795,10 @@ private:
 	const VkBufferCopy region;
 };
 
-class CmdImageToBufferCopy : public vk::CommandBuffer::Command
+class CmdCopyImageToBuffer : public vk::CommandBuffer::Command
 {
 public:
-	CmdImageToBufferCopy(vk::Image *srcImage, vk::Buffer *dstBuffer, const VkBufferImageCopy &region)
+	CmdCopyImageToBuffer(vk::Image *srcImage, vk::Buffer *dstBuffer, const VkBufferImageCopy &region)
 	    : srcImage(srcImage)
 	    , dstBuffer(dstBuffer)
 	    , region(region)
@@ -810,7 +810,7 @@ public:
 		srcImage->copyTo(dstBuffer, region);
 	}
 
-	std::string description() override { return "vkCmdImageToBufferCopy()"; }
+	std::string description() override { return "vkCmdCopyImageToBuffer()"; }
 
 private:
 	vk::Image *srcImage;
@@ -818,10 +818,10 @@ private:
 	const VkBufferImageCopy region;
 };
 
-class CmdBufferToImageCopy : public vk::CommandBuffer::Command
+class CmdCopyBufferToImage : public vk::CommandBuffer::Command
 {
 public:
-	CmdBufferToImageCopy(vk::Buffer *srcBuffer, vk::Image *dstImage, const VkBufferImageCopy &region)
+	CmdCopyBufferToImage(vk::Buffer *srcBuffer, vk::Image *dstImage, const VkBufferImageCopy &region)
 	    : srcBuffer(srcBuffer)
 	    , dstImage(dstImage)
 	    , region(region)
@@ -833,7 +833,7 @@ public:
 		dstImage->copyFrom(srcBuffer, region);
 	}
 
-	std::string description() override { return "vkCmdBufferToImageCopy()"; }
+	std::string description() override { return "vkCmdCopyBufferToImage()"; }
 
 private:
 	vk::Buffer *srcBuffer;
@@ -1615,7 +1615,7 @@ void CommandBuffer::copyBuffer(const Buffer *srcBuffer, Buffer *dstBuffer, uint3
 
 	for(uint32_t i = 0; i < regionCount; i++)
 	{
-		addCommand<::CmdBufferToBufferCopy>(srcBuffer, dstBuffer, pRegions[i]);
+		addCommand<::CmdCopyBuffer>(srcBuffer, dstBuffer, pRegions[i]);
 	}
 }
 
@@ -1630,7 +1630,7 @@ void CommandBuffer::copyImage(const Image *srcImage, VkImageLayout srcImageLayou
 
 	for(uint32_t i = 0; i < regionCount; i++)
 	{
-		addCommand<::CmdImageToImageCopy>(srcImage, dstImage, pRegions[i]);
+		addCommand<::CmdCopyImage>(srcImage, dstImage, pRegions[i]);
 	}
 }
 
@@ -1656,7 +1656,7 @@ void CommandBuffer::copyBufferToImage(Buffer *srcBuffer, Image *dstImage, VkImag
 
 	for(uint32_t i = 0; i < regionCount; i++)
 	{
-		addCommand<::CmdBufferToImageCopy>(srcBuffer, dstImage, pRegions[i]);
+		addCommand<::CmdCopyBufferToImage>(srcBuffer, dstImage, pRegions[i]);
 	}
 }
 
@@ -1668,7 +1668,7 @@ void CommandBuffer::copyImageToBuffer(Image *srcImage, VkImageLayout srcImageLay
 
 	for(uint32_t i = 0; i < regionCount; i++)
 	{
-		addCommand<::CmdImageToBufferCopy>(srcImage, dstBuffer, pRegions[i]);
+		addCommand<::CmdCopyImageToBuffer>(srcImage, dstBuffer, pRegions[i]);
 	}
 }
 
