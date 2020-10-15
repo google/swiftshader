09f224e90d563e7c9492e0bed0751f871f6d4d1a
Sean Risser <srisser@google.com>
2020-10-15 14:01:53 -0400

    Implement VK_KHR_shader_subgroup_extended_types
    This extension allows subgroup operations to use 8-bit, 16-bit, and
    64-bit integers as well as 16-bit floating point types if the
    implementation supports those types. So no work is required other
    than enabling the extension.
    
    Bug: b/169606946
    Tests: dEQP-VK.subgroups.arithmetic.*
    Change-Id: I824ad65a1c9d5f9fb98bc5769f97920de5815d0f
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/49448
    Tested-by: Sean Risser <srisser@google.com>
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>
    Kokoro-Result: kokoro <noreply+kokoro@google.com>

--- src/Vulkan/VkPhysicalDevice.cpp
+++ src/Vulkan/VkPhysicalDevice.cpp
@@ -212,6 +212,12 @@ static void getPhysicalDeviceImagelessFramebufferFeatures(T *features)
 }
 
 template<typename T>
+static void getPhysicalDeviceShaderSubgroupExtendedTypesFeatures(T *features)
+{
+	features->shaderSubgroupExtendedTypes = VK_TRUE;
+}
+
+template<typename T>
 static void getPhysicalDeviceVulkan12Features(T *features)
 {
 	features->samplerMirrorClampToEdge = VK_FALSE;
@@ -245,7 +251,7 @@ static void getPhysicalDeviceVulkan12Features(T *features)
 	features->scalarBlockLayout = VK_FALSE;
 	getPhysicalDeviceImagelessFramebufferFeatures(features);
 	features->uniformBufferStandardLayout = VK_FALSE;
-	features->shaderSubgroupExtendedTypes = VK_FALSE;
+	getPhysicalDeviceShaderSubgroupExtendedTypesFeatures(features);
 	getPhysicalDeviceSeparateDepthStencilLayoutsFeaturesKHR(features);
 	features->hostQueryReset = VK_FALSE;
 	features->timelineSemaphore = VK_FALSE;
@@ -310,6 +316,9 @@ void PhysicalDevice::getFeatures2(VkPhysicalDeviceFeatures2 *features) const
 				break;
 			case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_IMAGELESS_FRAMEBUFFER_FEATURES:
 				getPhysicalDeviceImagelessFramebufferFeatures(reinterpret_cast<VkPhysicalDeviceImagelessFramebufferFeatures *>(curExtension));
+				break;
+			case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SHADER_SUBGROUP_EXTENDED_TYPES_FEATURES_KHR:
+				getPhysicalDeviceShaderSubgroupExtendedTypesFeatures(reinterpret_cast<VkPhysicalDeviceShaderSubgroupExtendedTypesFeatures *>(curExtension));
 				break;
 			default:
 				LOG_TRAP("curExtension->pNext->sType = %s", vk::Stringify(curExtension->sType).c_str());
