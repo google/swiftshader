9559e5836cc1d7f8b59fc5a9ecde7e75f6b9051f
Nicolas Capens <nicolascapens@google.com>
2021-06-10 14:48:36 +0000

    Revert "Remove git submodules before rolling into Android"
    This reverts commit efe254de5d881c3d65030c6a5801d6969f9dde35.
    
    b/189557997 was addressed by removing .gitmodules in the AutoRoller:
    https://skia-review.googlesource.com/c/buildbot/+/415736
    
    Change-Id: Iec5ea73fce689f5286aedc486447ea0f498912b7
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/54868
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Jason Macnak <natsu@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>

--- tools/prepare_for_android.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is called by the SwiftShader into Android AutoRoller:
-# https://autoroll.skia.org/r/swiftshader-android
-
-# Remove git submodules
-# Android does not allow remote submodules (b/189557997)
-git config --file .gitmodules --get-regexp path | awk '{ system("git rm " $2) }'
-rm .gitmodules
