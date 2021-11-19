6e23c082b73a952d36c0cdb2f33f597166674f6d
Alexis Hetu <sugoi@google.com>
2021-11-19 00:01:09 -0500

    Make line stipple related tests pass
    While line stipple is unsupported, calling vkCmdSetLineStippleEXT is
    allowed and valid and both lineStippleFactor and lineStipplePattern
    are expected to be ignored, which they are, so I propose using WARN,
    instead of UNSUPPORTED in this case, so that we still get a useful
    warning, but also allow the code to proceed, since it is valid.
    
    Tests: dEQP-VK.dynamic_state.compute_transfer.single.*.line_stipple_ext.*
    Bug: b/207057280
    Change-Id: I6d1d6385cc208103910deb99144c08fa453f9336
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/59828
    Presubmit-Ready: Alexis Hétu <sugoi@google.com>
    Tested-by: Alexis Hétu <sugoi@google.com>
    Commit-Queue: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>

--- src/Vulkan/libVulkan.cpp
+++ src/Vulkan/libVulkan.cpp
@@ -3801,7 +3801,14 @@ VKAPI_ATTR void VKAPI_CALL vkCmdSetLineStippleEXT(VkCommandBuffer commandBuffer,
 	TRACE("(VkCommandBuffer commandBuffer = %p, uint32_t lineStippleFactor = %u, uint16_t lineStipplePattern = %u)",
 	      commandBuffer, lineStippleFactor, lineStipplePattern);
 
-	UNSUPPORTED("VkPhysicalDeviceLineRasterizationFeaturesEXT::stippled*Lines");
+	static constexpr uint16_t solidLine = 0xFFFFu;
+	if(lineStipplePattern != solidLine)
+	{
+		// VkPhysicalDeviceLineRasterizationFeaturesEXT::stippled*Lines are all set to VK_FALSE and,
+		// according to the Vulkan spec for VkPipelineRasterizationLineStateCreateInfoEXT:
+		// "If stippledLineEnable is VK_FALSE, the values of lineStippleFactor and lineStipplePattern are ignored."
+		WARN("vkCmdSetLineStippleEXT: line stipple pattern ignored : 0x%04X", lineStipplePattern);
+	}
 }
 
 VKAPI_ATTR void VKAPI_CALL vkCmdBeginDebugUtilsLabelEXT(VkCommandBuffer commandBuffer, const VkDebugUtilsLabelEXT *pLabelInfo)
