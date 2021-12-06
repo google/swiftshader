a9e93a8690f5d1504f150ebe5d57fa7138621f37
Nicolas Capens <capn@google.com>
2021-12-06 11:18:33 -0500

    Report storage usage capability for surface images
    VK_IMAGE_USAGE_STORAGE_BIT is practically universally supported for
    swapchain surfaces. It enables compute shaders to write directly to
    them.
    
    This usage capability is always enabled on Android, which provides a
    swapchain surface implementation based on the Android Hardware Buffer
    extension, instead of having the ICD provide it. Thus enabling this
    capability on other platforms ensures we test the mandatory underlying
    support with dEQP-VK.
    
    Bug: b/170414896
    Tests: dEQP-VK.image.swapchain_mutable.*
    Tests: dEQP-VK.wsi.*
    Change-Id: I15284b4c6f7105f95dce6d621876399066d24944
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/60528
    Kokoro-Result: kokoro <noreply+kokoro@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Sean Risser <srisser@google.com>

--- src/WSI/VkSurfaceKHR.cpp
+++ src/WSI/VkSurfaceKHR.cpp
@@ -195,7 +195,8 @@ void SurfaceKHR::setCommonSurfaceCapabilities(VkSurfaceCapabilitiesKHR *pSurface
 	    VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
 	    VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
 	    VK_IMAGE_USAGE_TRANSFER_DST_BIT |
-	    VK_IMAGE_USAGE_SAMPLED_BIT;
+	    VK_IMAGE_USAGE_SAMPLED_BIT |
+	    VK_IMAGE_USAGE_STORAGE_BIT;
 }
 
 }  // namespace vk
