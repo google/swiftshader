d0de9dcaaa4420103a5363a571e71298b6827dd8
Ben Clayton <bclayton@google.com>
2021-12-24 13:48:28 +0000

    third_party: Fix update-marl commit message
    It included `--squash` after the `./third_party/update-marl.sh` command, which is not what was typed, nor is it actually consumed.
    
    Bug: b/140546382
    Change-Id: Ie64fb8f2e7da147476a3f2b4f36da3df7e7433ea
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/61014
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>
    Kokoro-Result: Ben Clayton <bclayton@google.com>
    Tested-by: Ben Clayton <bclayton@google.com>
    Commit-Queue: Ben Clayton <bclayton@google.com>

--- third_party/update-marl.sh
+++ third_party/update-marl.sh
@@ -13,5 +13,5 @@ git subtree pull --prefix third_party/marl https://github.com/google/marl main -
 
 ALL_CHANGES=`git log -n 1 HEAD^2 | egrep '^(\s{4}[0-9a-f]{9}\s*.*)$'`
 HEAD_CHANGE=`echo "$ALL_CHANGES" | egrep '[0-9a-f]{9}' -o -m 1`
-LOG_MSG=`echo -e "Update Marl to $HEAD_CHANGE\n${REASON}\nChanges:\n$ALL_CHANGES\n\nCommands:\n    ./third_party/update-marl.sh --squash\n\nBug: b/140546382"`
+LOG_MSG=`echo -e "Update Marl to $HEAD_CHANGE\n${REASON}\nChanges:\n$ALL_CHANGES\n\nCommands:\n    ./third_party/update-marl.sh\n\nBug: b/140546382"`
 git commit --amend -m "$LOG_MSG"
