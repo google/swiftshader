d111ae28dd57fd3c484a3b18b5039764dbc37c91
Nicolas Capens <capn@google.com>
2021-12-28 14:48:28 +0000

    Regres: Build dEQP with asserts enabled
    This will potentially help catch some dEQP and/or SwiftShader bugs.
    
    Bug: b/141889636
    Change-Id: I177418be6f4a73da417121520a59887009e97336
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/40291
    Kokoro-Result: kokoro <noreply+kokoro@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Sean Risser <srisser@google.com>

--- tests/regres/cmd/regres/main.go
+++ tests/regres/cmd/regres/main.go
@@ -520,7 +520,7 @@ func (r *regres) getOrBuildDEQP(test *test) (deqpBuild, error) {
 
 		if err := shell.Shell(buildTimeout, r.cmake, buildDir,
 			"-DDEQP_TARGET=default",
-			"-DCMAKE_BUILD_TYPE=Release",
+			"-DCMAKE_BUILD_TYPE=RelWithAsserts",
 			".."); err != nil {
 			return deqpBuild{}, cause.Wrap(err, "Couldn't generate build rules for deqp %v @ %v", cfg.Remote, cfg.SHA)
 		}
