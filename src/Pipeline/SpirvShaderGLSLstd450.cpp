9fdbd96da7f9a9f8de3fc790401b5881024756bf
Nicolas Capens <capn@google.com>
2022-03-02 11:02:56 -0500

    Fix obtaining the RelaxedPrecision decoration from the result ID
    Unlike GLSL which determines the precision of operations based on the
    qualifiers of the operands, SPIR-V requires decorating the result of the
    instruction: https://www.khronos.org/registry/SPIR-V/specs/unified1/SPIRV.html#RelaxedPrecisionSection
    
    We were accidentally exercising the expected precision in the
    dEQP-VK.glsl.builtin.precision.* tests only because the instructions
    directly consumed interface variables, which can also be decorated with
    RelaxedPrecision to signify that loads and stores may reduce their
    precision (while still logically remaining 32-bit). It is incorrect to
    use this input parameter decoration to affect the precision of the
    instructions operating on them.
    
    Bug: b/222218659
    Change-Id: Ic2cedc53fc773269c73bf070248063bb6e932669
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/63649
    Kokoro-Result: kokoro <noreply+kokoro@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Sean Risser <srisser@google.com>

--- src/Pipeline/SpirvShaderGLSLstd450.cpp
+++ src/Pipeline/SpirvShaderGLSLstd450.cpp
@@ -620,7 +620,7 @@ SpirvShader::EmitResult SpirvShader::EmitExtGLSLstd450(InsnIterator insn, EmitSt
 	case GLSLstd450Asin:
 		{
 			auto val = Operand(this, state, insn.word(5));
-			Decorations d = GetDecorationsForId(insn.word(5));
+			Decorations d = GetDecorationsForId(insn.resultId());
 			for(auto i = 0u; i < type.componentCount; i++)
 			{
 				dst.move(i, sw::Asin(val.Float(i), d.RelaxedPrecision));
@@ -630,7 +630,7 @@ SpirvShader::EmitResult SpirvShader::EmitExtGLSLstd450(InsnIterator insn, EmitSt
 	case GLSLstd450Acos:
 		{
 			auto val = Operand(this, state, insn.word(5));
-			Decorations d = GetDecorationsForId(insn.word(5));
+			Decorations d = GetDecorationsForId(insn.resultId());
 			for(auto i = 0u; i < type.componentCount; i++)
 			{
 				dst.move(i, sw::Acos(val.Float(i), d.RelaxedPrecision));
@@ -768,7 +768,7 @@ SpirvShader::EmitResult SpirvShader::EmitExtGLSLstd450(InsnIterator insn, EmitSt
 	case GLSLstd450InverseSqrt:
 		{
 			auto val = Operand(this, state, insn.word(5));
-			Decorations d = GetDecorationsForId(insn.word(5));
+			Decorations d = GetDecorationsForId(insn.resultId());
 
 			for(auto i = 0u; i < type.componentCount; i++)
 			{
