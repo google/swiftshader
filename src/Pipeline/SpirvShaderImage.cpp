cfcb44322bccc5212d6cb0d850dd4b94f272f4f9
Antto Mäkinen <antto.makinen@siru.fi>
2022-06-14 11:57:23 +0300

    Fix the linear-to-sRGB conversion on SRGB storage image writes
    According to the Vulkan spec, a linear-to-SRGB conversion shouldn't
    be applied on the A component. This commit fixes SwiftShader
    to perform the conversion on the R, G, and B components only.
    
    Bug: b/236055502
    Change-Id: I943c7cc1b0b3b6193e28a8688660a25ae5faced0
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/66488
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Reviewed-by: Nicolas Capens <nicolascapens@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Kokoro-Result: kokoro <noreply+kokoro@google.com>

--- src/Pipeline/SpirvShaderImage.cpp
+++ src/Pipeline/SpirvShaderImage.cpp
@@ -1228,7 +1228,7 @@ void SpirvShader::WriteImage(ImageInstructionSignature instruction, Pointer<Byte
 		packed[0] = (SIMD::UInt(Round(Min(Max(linearToSRGB(As<SIMD::Float>(texel[2])), SIMD::Float(0.0f)), SIMD::Float(1.0f)) * SIMD::Float(255.0f)))) |
 		            ((SIMD::UInt(Round(Min(Max(linearToSRGB(As<SIMD::Float>(texel[1])), SIMD::Float(0.0f)), SIMD::Float(1.0f)) * SIMD::Float(255.0f)))) << 8) |
 		            ((SIMD::UInt(Round(Min(Max(linearToSRGB(As<SIMD::Float>(texel[0])), SIMD::Float(0.0f)), SIMD::Float(1.0f)) * SIMD::Float(255.0f)))) << 16) |
-		            ((SIMD::UInt(Round(Min(Max(linearToSRGB(As<SIMD::Float>(texel[3])), SIMD::Float(0.0f)), SIMD::Float(1.0f)) * SIMD::Float(255.0f)))) << 24);
+		            ((SIMD::UInt(Round(Min(Max(As<SIMD::Float>(texel[3]), SIMD::Float(0.0f)), SIMD::Float(1.0f)) * SIMD::Float(255.0f)))) << 24);
 		break;
 	case VK_FORMAT_R8G8B8A8_SNORM:
 	case VK_FORMAT_A8B8G8R8_SNORM_PACK32:
