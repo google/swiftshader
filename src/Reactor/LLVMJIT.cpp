8c9c9f7a53a55519c6e2f2baff6adfd085a1deeb
Nicolas Capens <capn@google.com>
2022-09-07 10:24:08 -0400

    Update renamed LLVM sanitizer passes
    This integrates cl/472657058 and updates the LLVM-Submodule backend
    for Reactor to the matching revision.
    
    Bug: b/165000222
    Change-Id: Ibc7329f6202cdb4846663c9ff024c386bc65c25a
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/68048
    Tested-by: Nicolas Capens <nicolascapens@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>

--- src/Reactor/LLVMJIT.cpp
+++ src/Reactor/LLVMJIT.cpp
@@ -935,12 +935,11 @@ void JITBuilder::runPasses()
 	{
 		llvm::MemorySanitizerOptions msanOpts(0 /* TrackOrigins */, false /* Recover */, false /* Kernel */, true /* EagerChecks */);
 		pm.addPass(llvm::ModuleMemorySanitizerPass(msanOpts));
-		pm.addPass(llvm::createModuleToFunctionPassAdaptor(llvm::MemorySanitizerPass(msanOpts)));
 	}
 
 	if(__has_feature(address_sanitizer) && ADDRESS_SANITIZER_INSTRUMENTATION_SUPPORTED)
 	{
-		pm.addPass(llvm::ModuleAddressSanitizerPass(llvm::AddressSanitizerOptions{}));
+		pm.addPass(llvm::AddressSanitizerPass(llvm::AddressSanitizerOptions{}));
 	}
 
 	pm.run(*module, mam);
