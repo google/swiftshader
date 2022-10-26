c6042c6052ec88a4564f294177e885808d47f66c
Nicolas Capens <capn@google.com>
2022-10-26 06:25:19 +0000

    Regres: default to 100 tests per process
    https://swiftshader-review.googlesource.com/c/SwiftShader/+/69209 shows the
    speedup achieved by this change: from 2h1m to 7m45s.
    
    More than 100 tests per process doesn't have a significant speedup effect,
    while 10 is about 2x slower.
    
    If 1% of tests crash we spawn roughly the same number of processes running
    1 test as before. In other words it would take about 2h to run in that
    case. Higher percentage crashes wouldn't slow it down further since they'd
    often fall into the same "bucket". At 1000 tests per process it would only
    take 0.1% crashes to reach maximum slowdown so this is not worth the
    marginal speedup.
    
    Bug: b/253530501
    Change-Id: Iaa540755082a87b7d1cd3da23d5a72de5948f40e
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/69309
    Kokoro-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Alexis Hétu <sugoi@google.com>
    Tested-by: Nicolas Capens <nicolascapens@google.com>

--- tests/regres/cmd/regres/main.go
+++ tests/regres/cmd/regres/main.go
@@ -90,7 +90,7 @@ var (
 	githubPass      = flag.String("gh-pass", "$SS_GITHUB_PASS", "github password for posting coverage results")
 	keepCheckouts   = flag.Bool("keep", false, "don't delete checkout directories after use")
 	dryRun          = flag.Bool("dry", false, "don't post regres reports to gerrit")
-	maxTestsPerProc = flag.Int("max-tests-per-proc", 1, "maximum number of tests running in a single process")
+	maxTestsPerProc = flag.Int("max-tests-per-proc", 100, "maximum number of tests running in a single process")
 	maxProcMemory   = flag.Uint64("max-proc-mem", shell.MaxProcMemory, "maximum virtual memory per child process")
 	dailyNow        = flag.Bool("dailynow", false, "Start by running the daily pass")
 	dailyOnly       = flag.Bool("dailyonly", false, "Run only the daily pass")
