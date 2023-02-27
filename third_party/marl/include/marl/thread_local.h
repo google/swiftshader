0ba0b45490cd209448c5f976f41b34a746c9de5d
Ben Clayton <bclayton@google.com>
2023-02-27 20:47:01 +0000

    marl: Remove spurious semicolons
    Bug: chromium:1337597
    Change-Id: I132c441f0b44035ba4d4b7b14da1953eadcc09e9
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/70908
    Kokoro-Result: kokoro <noreply+kokoro@google.com>
    Presubmit-Ready: Ben Clayton <bclayton@google.com>
    Tested-by: Ben Clayton <bclayton@google.com>
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>
    Commit-Queue: Ben Clayton <bclayton@google.com>

--- third_party/marl/include/marl/thread_local.h
+++ third_party/marl/include/marl/thread_local.h
@@ -33,7 +33,7 @@ class ThreadLocal {
   inline ThreadLocal(T v) {
     pthread_key_create(&key, NULL);
     pthread_setspecific(key, v);
-  };
+  }
   inline ~ThreadLocal() { pthread_key_delete(key); }
   inline operator T() const { return static_cast<T>(pthread_getspecific(key)); }
   inline ThreadLocal& operator=(T v) {
@@ -45,15 +45,15 @@ class ThreadLocal {
   pthread_key_t key;
 };
 
-#define MARL_DECLARE_THREAD_LOCAL(TYPE, NAME) static ThreadLocal<TYPE> NAME;
+#define MARL_DECLARE_THREAD_LOCAL(TYPE, NAME) static ThreadLocal<TYPE> NAME
 #define MARL_INSTANTIATE_THREAD_LOCAL(TYPE, NAME, VALUE) \
-  ThreadLocal<TYPE> NAME = VALUE;
+  ThreadLocal<TYPE> NAME = VALUE
 
 #else
 
-#define MARL_DECLARE_THREAD_LOCAL(TYPE, NAME) static thread_local TYPE NAME;
+#define MARL_DECLARE_THREAD_LOCAL(TYPE, NAME) static thread_local TYPE NAME
 #define MARL_INSTANTIATE_THREAD_LOCAL(TYPE, NAME, VALUE) \
-  thread_local TYPE NAME = VALUE;
+  thread_local TYPE NAME = VALUE
 
 #endif
 
