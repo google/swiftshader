a87fa39f64462d4c3ef607d1055366370f7bddd6
David Neto <dneto@google.com>
2025-01-23 18:34:28 -0500

    tests/kokoro: use clang-format 13
    It's in the Docker image
    
    Change-Id: I34c41d69d691fc2e88cb268e143b5c9a14d38687
    Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/74848
    Reviewed-by: Shahbaz Youssefi <syoussefi@google.com>
    Tested-by: Shahbaz Youssefi <syoussefi@google.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@google.com>

--- tests/kokoro/gcp_ubuntu/check_style.sh
+++ tests/kokoro/gcp_ubuntu/check_style.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
+. /bin/using.sh
+
 set -x # Display commands being run.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 
-# Download Clang tar
-CLANG_PACKAGE="clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04"
-curl -L https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/${CLANG_PACKAGE}.tar.xz > /tmp/clang.tar.xz
-# Verify Clang tar
-sudo apt-get install pgpgpg
-gpg --import "${SCRIPT_DIR}/tstellar-gpg-key.asc"
-gpg --verify "${SCRIPT_DIR}/${CLANG_PACKAGE}.tar.xz.sig" /tmp/clang.tar.xz
-if [ $? -ne 0 ]
-then
-  echo "clang download failed PGP check"
-  exit 1
-fi
-
-set -e # Fail on any error
-
-# Untar into tmp
-mkdir /tmp/clang
-tar -xf /tmp/clang.tar.xz -C /tmp/clang
+using clang-13.0.1
 
 # Set up env vars
-export CLANG_FORMAT=/tmp/clang/${CLANG_PACKAGE}/bin/clang-format
+export CLANG_FORMAT=`which clang-format`
 
 # Run presubmit tests
 cd git/SwiftShader
